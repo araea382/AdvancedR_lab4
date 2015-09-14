2802306547216dfb9f2f73f617f483aeb34c74d1
unknown <Clara.Schartner@gmail.com>
2015-09-14 22:50:33 +0200

    class

--- lab4/R/linreg.R
+++ lab4/R/linreg.R
@@ -2,16 +2,18 @@ linreg <- function(formula, data){
     X <- model.matrix(formula, data)
 
     #like this maybe? takes all the arguments that are not and therefore in the X matrix
-    y <- all.vars(formula)[!(all.vars(formula) %in% colnames(X))]
+    y <- as.matrix(data[all.vars(formula)[!(all.vars(formula) %in% colnames(X))]])
     
     Q <- qr.Q(qr(X))
     R <- qr.R(qr(X))
     
     ans <- solve(R) %*% t(Q) %*% y
+    ans<-structure(list(ans), class = "linreg")
+    
     return(ans)
 }
 
-
+linreg <- structure(list(), class = "linreg")
 ##Test outside the function with faithful data
 X <- model.matrix(eruptions~waiting, data = faithful)
 y <- as.matrix(faithful[,1, drop = FALSE])
@@ -27,7 +29,6 @@ beta <- solve(R) %*% t(Q) %*% y
 ##the coefficients is correct!
 m <- lm(eruptions~waiting, faithful)
 m
-
 #the fitted values 
 fit <- X %*% beta
 
@@ -41,3 +42,4 @@ df <- n - p
 
 #the residual variance
 var_res <- as.vector((t(res) %*% res) / df)
+
--- lab4/tests/testthat/tests.R
+++ lab4/tests/testthat/tests.R
@@ -1,9 +1,16 @@
+#this test will fail because they are not absolutely identical.
+
 data("faithful")
+m<-lm(eruptions~waiting,faithful)==
+ n<- linreg(eruptions~waiting,faithful)
 test_that("incorrect",{
-  expect_equal(lm(eruptions~waiting,faithful)==linreg(eruptions~waiting,faithful),TRUE)
+  expect_equal(sum(as.numeric(unlist(m[1]))-as.numeric(unlist(n))),0)
 })
+
+
 data("iris")
+lm.iris<-lm(Sepal.Length~Sepal.Width+Petal.Length^2,iris)==
+lin.iris<-  linreg(Sepal.Length~Sepal.Width+Petal.Length^2,iris)
 test_that("cubic is correct",{
-expect_equal(lm(Sepal.Length~Sepal.Width+Petal.Length^2,iris)==
-  linreg(Sepal.Length~Sepal.Width+Petal.Length^2,iris),TRUE)
+expect_equal(sum(as.numeric(unlist(lm.iris[1]))-as.numeric(unlist(lin.iris))),0)
 })
