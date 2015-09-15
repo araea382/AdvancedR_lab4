d42d5c0a0e0e5b184da30865e6ee9c2698683a66
Araya Eamrurksiri <araea382@student.liu.se>
2015-09-15 22:51:34 +0200

    add variance of coefficients and t-values for each coefficients

--- lab4/R/linreg.R
+++ lab4/R/linreg.R
@@ -2,22 +2,22 @@ library(ggplot2)
 
 linreg <- function(formula, data){
     X <- model.matrix(formula, data)
-
-    #like this maybe? takes all the arguments that are not and therefore in the X matrix
     y <- as.matrix(data[all.vars(formula)[!(all.vars(formula) %in% colnames(X))]])
     
     Q <- qr.Q(qr(X))
     R <- qr.R(qr(X))
     
     ans <- solve(R) %*% t(Q) %*% y
-    beta<-ans
+    beta <- as.vector(ans)
+    
     #the fitted values 
     fit <- X %*% beta
     
     #the residuals
     res <- y - fit
-    fit.res<-data.frame(fit,res)
-    names(fit.res)<-c("fit","res")
+    fit.res <- data.frame(fit, res)
+    names(fit.res) <- c("fit", "res")
+    
     #the degree of freedoms
     n <- nrow(X)
     p <- ncol(X)
@@ -26,50 +26,35 @@ linreg <- function(formula, data){
     #the residual variance
     var_res <- as.vector((t(res) %*% res) / df)
     
-    ans<-structure(ans, class = "linreg")
+    #the variance of the regression coefficients
+    var_tmp <- var_res * solve(t(R) %*% R)
+    var_coeff <- numeric()
+    for(i in 1:nrow(var_tmp)){
+        var_coeff <- c(var_coeff,var_tmp[i,i])
+    }
+    
+    #the t-values for each coefficient
+    t_values <- numeric()
+    for(i in 1:length(beta)){
+        t_values <- c(t_values, beta[i] / sqrt(var_coeff[i]))
+    }
+    
+    #p-value -> use pt()
+    
+    
+    ans <- structure(ans, class = "linreg")
     
     #below other option which includes the formula, however as a list.
     print.linreg <- function(ans) {
       cat(colnames(X),"\n")
       cat(ans[1:2],"\n")
-#cat("linreg(",as.call(as.list(formula)),")","\n") #cannot get it to print the formula
+    #cat("linreg(",as.call(as.list(formula)),")","\n") #cannot get it to print the formula
     }
     
     print.linreg(ans)
 }
 
-p<-ggplot(data=fit.res,aes(x=fit,y=res))+geom_point()+ add_cat()
-
-
-linreg <- structure(list(), class = "linreg")
-##Test outside the function with faithful data
-X <- model.matrix(eruptions~waiting, data = faithful)
-y <- as.matrix(faithful[,1, drop = FALSE])
-
-Q <- qr.Q(qr(X))
-R <- qr.R(qr(X))
-
-#regresions coefficients: intercept and slope 
-beta <- solve(R) %*% t(Q) %*% y
-#beta <- as.vector(beta)
-
-####check with lm
-##the coefficients is correct!
-m <- lm(eruptions~waiting, faithful)
-m
-#the fitted values 
-fit <- X %*% beta
-
-#the residuals
-res <- y - fit
-
-#the degree of freedoms
-n <- nrow(X)
-p <- ncol(X)
-df <- n - p
-
-#the residual variance
-var_res <- as.vector((t(res) %*% res) / df)
+p <- ggplot(data=fit.res,aes(x=fit,y=res))+geom_point()+ add_cat()
 
 
 #or else
